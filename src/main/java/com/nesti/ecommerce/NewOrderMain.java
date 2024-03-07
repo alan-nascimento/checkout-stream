961be91f04d476f484cdce469df0dcd8f4454163
Alan Nascimento <alan.silva.n@hotmail.com>
2024-03-07 18:55:29 -0300

    feat: add producer

--- src/main/java/com/nesti/ecommerce/NewOrderMain.java
+++ src/main/java/com/nesti/ecommerce/NewOrderMain.java
@@ -13,12 +13,22 @@ public class NewOrderMain {
     var producer = new KafkaProducer<String, String>(properties());
     var message = "Apple Watch Ultra 2,8000.00";
     var record = new ProducerRecord<>("CHECKOUT", message, message);
-    producer.send(record);
+    try {
+      producer.send(record, (data, ex) -> {
+        if (ex != null) {
+          ex.printStackTrace();
+          return;
+        }
+        System.out.println("Success: " + data.topic() + " " + data.partition() + " " + data.offset());
+      }).get();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
   private static Properties properties() {
     var properties = new Properties();
-    properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+    properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
     properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
     properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
     return properties;
